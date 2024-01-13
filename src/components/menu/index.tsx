d2d414684d6bdab78c62f6928dd751a3ed7d9c92
johncarlofstackdev <johncarlo.fullstackdev@gmail.com>
2024-01-13 23:57:54 +0800

    Fixed deployment error

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { FontAwesomeIcon, IconProp } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { 
   faFileAlt,
   faQuestionCircle,
@@ -13,7 +13,7 @@ import {
 
 interface MenuItem {
   path: string;
-  icon: IconDefinition | IconProp;
+  icon: IconDefinition;
 }
 
 interface MenuProps {
