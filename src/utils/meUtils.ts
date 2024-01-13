ef8fdffb84d3ae8796f7b7dd23eed53963d5a80a
johncarlofstackdev <johncarlo.fullstackdev@gmail.com>
2024-01-14 02:41:05 +0800

    Improvise function age that will include the month and day

--- src/utils/meUtils.ts
+++ src/utils/meUtils.ts
@@ -1,10 +1,23 @@
 
 export const Age = () => {
     const birthYear = 1999;
-    // Calculate age based on current year
-    const currentYear = new Date().getFullYear();
-    return ( currentYear - birthYear );
-}
+    const birthMonth = 7;
+    const birthDay = 14;
+  
+    const today = new Date();
+    const currentYear = today.getFullYear();
+    const currentMonth = today.getMonth() + 1; // Adding 1 because months are zero-based
+    const currentDay = today.getDate();
+  
+    let age = currentYear - birthYear;
+  
+    // Adjust age based on the birth month and day
+    if (currentMonth < birthMonth || (currentMonth === birthMonth && currentDay < birthDay)) {
+      age--;
+    }
+  
+    return age;
+};  
 
 export const myResumeEmbedURL = 'https://www.canva.com/design/DAFl3W_qSJU/view?embed';
 export const myResumeFilePath = '/files/John Carlo A. Ylanan - Resume.pdf';
